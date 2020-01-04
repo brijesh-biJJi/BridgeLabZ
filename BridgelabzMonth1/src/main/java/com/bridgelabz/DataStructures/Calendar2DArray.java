7ce7808f74d3f922249d199d74984e3440e58425
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-04 09:38:23 +0530

    [Brijesh] Add. display method to Calendar2DArray Class

--- BridgelabzMonth1/src/main/java/com/bridgelabz/DataStructures/Calendar2DArray.java
+++ BridgelabzMonth1/src/main/java/com/bridgelabz/DataStructures/Calendar2DArray.java
@@ -1,14 +1,14 @@
-package Calendar;
+package com.bridgelabz.DataStructures;
 
 import com.bridgelabz.inputscanner.InputScanner;
 import com.bridgelabz.utility.Utility;
 
 public class Calendar2DArray 
-{	
-	static int[][] cal=new int[6][7];
+{
+static int[][] cal=new int[6][7];
 	
 	//storeDate is used to store the date in 2D Array
-	public static void storeDate(int firstDay,int mm)
+	public static int[][] storeDate(int firstDay,int mm)
 	{
 		int start=1;
 		for (int i = 0; i < 6; i++)
@@ -16,13 +16,16 @@ public class Calendar2DArray
 	
 			for (int j = 0; j < 7; j++) 
 			{
-				cal[i][j]=firstDay-- >0 ? 0 : start++;
-				
+				if(start<=mm)
+					cal[i][j]=firstDay-- >0 ? 0 : start++;	
 			}
-			if(start>=mm)
-				break;
 		}
-		
+		return cal;   
+	}
+	
+	//Display the calendar
+	public static void displayCalendar(int[][] cal)
+	{
 		String nameOfTheWeeks = "Sun\t" + "Mon\t" + "Tue\t" + "Wed\t" + "Thu\t" + "Fri\t" + "Sat\t";
         System.out.println(nameOfTheWeeks);
 
@@ -35,9 +38,7 @@ public class Calendar2DArray
 		    }
 		    System.out.println();
 		}
-		    
 	}
-	
 	
 	public static void main(String[] args) 
 	{
@@ -51,6 +52,7 @@ public class Calendar2DArray
 		int[] noOfDay=Utility.noOfDay();
 		int firstDay=noOfDay[0]%7;
 		int mm=noOfDay[1];
-		storeDate(firstDay,mm);
+		int[][] calendar=storeDate(firstDay,mm);
+		displayCalendar(calendar);
 	}
 }
