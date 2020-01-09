cb6d0ad0b78867ad062f8f96e16ce8ebd2ba723b
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-09 16:28:53 +0530

    [Brijesh] Add. Created StockAccountManagement Class

---
+++ OOPS/src/main/java/com/bridgelabz/Controller/StockAccountManagement.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.Controller;
+
+public class StockAccountManagement {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+System.out.println("hello");
+	}
+
+}
