19c429ef3a8e3c97ba01d89be1d9f31a0cf2f888
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-11 12:51:19 +0530

    [brijesh] Modified. JsonInventoryDatamanagementImpl class and inventoryData.json

--- OOPS/src/main/java/com/bridgelabz/ServiceImplementation/JsonInventoryDataManagementImpl.java
+++ OOPS/src/main/java/com/bridgelabz/ServiceImplementation/JsonInventoryDataManagementImpl.java
@@ -2,7 +2,13 @@ package com.bridgelabz.ServiceImplementation;
 
 
 
+import java.io.*;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import com.bridgelabz.Model.JsonInventoryDataManagementModel;
 import com.bridgelabz.Services.IJsonInventoryDataManagement;
@@ -12,11 +18,17 @@ public class JsonInventoryDataManagementImpl implements IJsonInventoryDataManage
 {
 	JsonInventoryDataManagementModel inventoryModel=new JsonInventoryDataManagementModel();
 
+	JSONParser parser=new JSONParser();
 	
 	@Override
 	public void addInventoryItems() 
 	{
-		JSONParser parser=new JSONParser();
+		
+		
+		
+		
+		
+		
 		double total=0;
 		System.out.println("Enter the Item name to add..");
 		String itemName=InputScanner.inputWord();
@@ -37,11 +49,37 @@ public class JsonInventoryDataManagementImpl implements IJsonInventoryDataManage
 	@Override
 	public void saveInventoryItems() 
 	{
-		
-		System.out.println("Inventory Item Name : "+inventoryModel.getName());
+		try 
+		{
+			//Object inventoryObj=parser.parse(new FileReader("inventoryData.json"));
+			//JSONObject jsonObject=(JSONObject)inventoryObj;
+			JSONObject jsonObject=(JSONObject) parser.parse(new FileReader("/home/user/Desktop/GitBridgelabz/BridgeLabZ/OOPS/src/main/java/com/bridgelabz/Repo/inventoryData.json"));
+			
+			JSONArray rice=(JSONArray)jsonObject.get("rice");
+			JSONArray wheat=(JSONArray)jsonObject.get("wheat");
+						
+			Iterator<Object> iter=rice.iterator();
+			while(iter.hasNext())
+			{
+				System.out.print("Rice : ");
+				System.out.println(iter.next());
+			}
+			Iterator<Object> iter1=wheat.iterator();
+			while(iter1.hasNext())
+			{
+				System.out.print("Wheat : ");
+				System.out.println(iter1.next());
+			}
+			
+		} 
+		catch (FileNotFoundException e) { e.printStackTrace();}
+		catch (IOException e) { e.printStackTrace();}
+		catch (ParseException e) { e.printStackTrace();}
+		catch (Exception e) { e.printStackTrace();}
+		/*System.out.println("Inventory Item Name : "+inventoryModel.getName());
 		System.out.println("Inventory Item weight : "+inventoryModel.getWeight());
 		System.out.println("Inventory Item price_per_kg : "+inventoryModel.getPrice_per_kg());
-		System.out.println("Inventory Total Amount : "+inventoryModel.getTotal());
+		System.out.println("Inventory Total Amount : "+inventoryModel.getTotal());*/
 	}
 	
 }
