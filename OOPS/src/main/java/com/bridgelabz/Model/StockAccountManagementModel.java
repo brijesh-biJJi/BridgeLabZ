c556b389d3265a4893f70643bf9a62f65988872a
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-13 14:49:31 +0530

    [Brijesh] Add. Created StockAccountManagementModel class

---
+++ OOPS/src/main/java/com/bridgelabz/Model/StockAccountManagementModel.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.Model;
+
+public class StockAccountManagementModel 
+{
+	private String name;
+	private long share;
+	private long  price;
+	private long value;
+	
+	public String getName() 
+	{
+		return name;
+	}
+	public void setName(String name) 
+	{
+		this.name = name;
+	}
+	public long getShare() 
+	{
+		return share;
+	}
+	public void setShare(long share) 
+	{
+		this.share = share;
+	}
+	public long getPrice() 
+	{
+		return price;
+	}
+	public void setPrice(long price) 
+	{
+		this.price = price;
+	}
+	public long getValue() 
+	{
+		return value;
+	}
+	public void setValue(long value) 
+	{
+		this.value = value;
+	}
+}
