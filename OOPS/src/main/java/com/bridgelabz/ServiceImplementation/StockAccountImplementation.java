4fb8576ef46908254212ecc5d7d9c8370777f9db
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-13 17:44:31 +0530

    [Brijesh] Add. Created StockAccountImplementation class

---
+++ OOPS/src/main/java/com/bridgelabz/ServiceImplementation/StockAccountImplementation.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.ServiceImplementation;
+
+import com.bridgelabz.Services.IStockAccount;
+
+public class StockAccountImplementation implements IStockAccount
+{
+
+	@Override
+	public double valueOF() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void buy(int amount, String symbol) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sell(int amount, String symbol)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void save(String fileName) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printReport() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
