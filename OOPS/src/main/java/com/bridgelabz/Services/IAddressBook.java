e6b079fbdd7e273bcf2cd379d7f434b03586c483
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-16 15:09:39 +0530

    [Brijesh] Modified. IAddressBook interface

--- OOPS/src/main/java/com/bridgelabz/Services/IAddressBook.java
+++ OOPS/src/main/java/com/bridgelabz/Services/IAddressBook.java
@@ -7,5 +7,5 @@ public interface IAddressBook
 	void addAddressBook();
 	void editAddressBook(String personObj);
 	void deleteAddressBook(String personObj);
-	void selectAddressBook(String personObj);
+	void displayAddressBook();
 }
