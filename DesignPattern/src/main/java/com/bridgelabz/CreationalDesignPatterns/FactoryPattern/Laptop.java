274764af34c3c4eaa8fb7943b475e2678dd576f5
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-18 14:37:55 +0530

    [Brijesh] Add. Created Laptop class in Factory Design pattern

---
+++ DesignPattern/src/main/java/com/bridgelabz/CreationalDesignPatterns/FactoryPattern/Laptop.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.CreationalDesignPatterns.FactoryPattern;
+
+public class Laptop extends Computer
+{
+	String Ram,HDD,CPU;
+	
+	/**
+	 * Parameterized Constructor of Laptop Class
+	 * @param ram
+	 * @param hdd
+	 * @param cpu
+	 */
+	Laptop(String ram,String hdd,String cpu)
+	{
+		this.Ram=ram;
+		this.HDD=hdd;
+		this.CPU=cpu;
+	}
+	
+	@Override
+	public String getRam() 
+	{
+		return this.Ram;
+	}
+
+	@Override
+	public String getHDD() 
+	{
+		return this.HDD;
+	}
+
+	@Override
+	public String getCPU() 
+	{
+		return this.CPU;
+	}
+}
