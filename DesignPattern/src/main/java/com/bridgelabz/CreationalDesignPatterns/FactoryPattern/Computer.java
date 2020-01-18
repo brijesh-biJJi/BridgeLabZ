01a2f27d4bcbdfd2b33ac1693bbcc8ad51dfb5c1
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-18 15:03:28 +0530

    [Brijesh] Add. Created TestComputer class in Factory Design pattern

--- DesignPattern/src/main/java/com/bridgelabz/CreationalDesignPatterns/FactoryPattern/Computer.java
+++ DesignPattern/src/main/java/com/bridgelabz/CreationalDesignPatterns/FactoryPattern/Computer.java
@@ -6,4 +6,10 @@ public abstract class Computer
 	public abstract String getRam();
 	public abstract String getHDD();
 	public abstract String getCPU();
+	
+	@Override
+	public String toString()
+	{
+		return "RAM : "+this.getRam()+"\nHardDiskDrive : "+this.getHDD()+"\nCPU : "+this.getCPU();
+	}
 }
--- DesignPattern/src/main/java/com/bridgelabz/CreationalDesignPatterns/FactoryPattern/TestComputer.java
+++ DesignPattern/src/main/java/com/bridgelabz/CreationalDesignPatterns/FactoryPattern/TestComputer.java
@@ -1,11 +1,25 @@
 package com.bridgelabz.CreationalDesignPatterns.FactoryPattern;
 
+import com.bridgelabz.InputScanner.InputScanner;
+
 public class TestComputer {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Enter the type of the Computer you want to Buy...!");
+		String type=InputScanner.inputWord();
 		
+		Computer c=ComputerFactory.getComputer(type, "4 GB", "1 TB", "2.9 GHz");
+		if(c !=null)
+		{
+			System.out.println("Factory "+type+" Config :");
+			/*System.out.println("*******************");
+			System.out.println("Ram : "+c.getRam());
+			System.out.println("HardDiskDrive : "+c.getHDD());
+			System.out.println("CPU : "+c.getCPU());*/
+			
+			System.out.println(c);
+		}
 	}
 
 }
