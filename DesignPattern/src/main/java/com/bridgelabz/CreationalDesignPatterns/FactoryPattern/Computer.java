53ab8e8ecc95ecf2509ff009275b665652626744
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-18 14:26:26 +0530

    [Brijesh] Add. Created Computer class in Factory Design pattern

---
+++ DesignPattern/src/main/java/com/bridgelabz/CreationalDesignPatterns/FactoryPattern/Computer.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.CreationalDesignPatterns.FactoryPattern;
+
+public abstract class Computer
+{
+	public abstract String getRam();
+	public abstract String getHDD();
+	public abstract String getCPU();
+}
