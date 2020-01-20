613515f0c77c228ea0970f3c94c83de257859dad
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-20 18:48:24 +0530

    [Brijesh] Add. Implemented SocketObjectAdapterImpl class

--- DesignPattern/src/main/java/com/bridgelabz/StructuralDesignPatterns/AdapterPattern/AdapterPatternTest.java
+++ DesignPattern/src/main/java/com/bridgelabz/StructuralDesignPatterns/AdapterPattern/AdapterPatternTest.java
@@ -8,6 +8,22 @@ public class AdapterPatternTest
 	public static void main(String[] args) 
 	{
 		testClassAdapter();
+		testObjectAdapter();
+	}
+
+	private static void testObjectAdapter() 
+	{
+		ISocketAdapter socketAdapter=new SocketObjectAdapterImpl();
+		Volt v3=getVolt(socketAdapter,3);
+		Volt v12=getVolt(socketAdapter, 12);
+		Volt v120=getVolt(socketAdapter, 120);
+		
+		System.out.println("Test Object Adapter");
+		System.out.println("*******************");
+		System.out.println("V3 volts using Class Adapter :"+v3.getVolts());
+		System.out.println("V12 volts using Class Adapter :"+v12.getVolts());
+		System.out.println("V120 volts using Class Adapter :"+v120.getVolts());
+	
 	}
 
 	private static void testClassAdapter() 
@@ -17,9 +33,12 @@ public class AdapterPatternTest
 		Volt v12=getVolt(socketAdapter, 12);
 		Volt v120=getVolt(socketAdapter, 120);
 		
+		System.out.println("Test Class Adapter");
+		System.out.println("******************");
 		System.out.println("V3 volts using Class Adapter :"+v3.getVolts());
 		System.out.println("V12 volts using Class Adapter :"+v12.getVolts());
 		System.out.println("V120 volts using Class Adapter :"+v120.getVolts());
+		System.out.println();
 	}
 
 	private static Volt getVolt(ISocketAdapter socketAdapter, int i) 
