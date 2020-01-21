dffa52f6421d2a93d5f55049f3695367cdd7b5d2
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-21 11:38:37 +0530

    [Brijesh] Add. Implemented HelperFacade  class

--- DesignPattern/src/main/java/com/bridgelabz/StructuralDesignPatterns/FacadePattern/FacadePatternTest.java
+++ DesignPattern/src/main/java/com/bridgelabz/StructuralDesignPatterns/FacadePattern/FacadePatternTest.java
@@ -2,6 +2,8 @@ package com.bridgelabz.StructuralDesignPatterns.FacadePattern;
 
 import java.sql.Connection;
 
+import com.bridgelabz.StructuralDesignPatterns.FacadePattern.HelperFacade.*;
+
 public class FacadePatternTest {
 
 	public static void main(String[] args) 
@@ -9,19 +11,31 @@ public class FacadePatternTest {
 		String tableName="Employee";
 		
 		/**
+		 * Generating MySql HTML report and Oracle PDF report without using Facade
+		 */
+		System.out.println("Generating MySql HTML report and Oracle PDF report without using Facade");
+		/**
 		 * Get Mysql DB Connection
 		 */
 		Connection conM=MySqlHelper.getMySqlDBConnection();
 		MySqlHelper mySqlHelper=new MySqlHelper();
-		mySqlHelper.generateMySqlPDFReport(tableName, conM);
+		mySqlHelper.generateMySqlHTMLReport(tableName, conM);
 		
 		/**
 		 * Get Oracle DB Connection
 		 */
 		Connection conO=OracleHelper.getOracleDBConnection();
 		OracleHelper oracleHelper=new OracleHelper();
-		oracleHelper.generateOracleHTMLReport(tableName, conO);
+		oracleHelper.generateOraclePDFReport(tableName, conO);
 		
+		
+
+		/**
+		 * Generating MySql HTML report and Oracle PDF report using Facade
+		 */
+		System.out.println("\nGenerating MySql HTML report and Oracle PDF report using Facade\n");
+		HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
+		HelperFacade.generateReport(HelperFacade.DBTypes.ORACLE,HelperFacade.ReportTypes.PDF, tableName);
 	}
 
 }
