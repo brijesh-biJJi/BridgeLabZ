4146deb0c2c266ad968b2189edda941ec7a5426e
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-21 10:51:38 +0530

    [Brijesh] Add. Implemented MySqlHelper class

---
+++ DesignPattern/src/main/java/com/bridgelabz/StructuralDesignPatterns/FacadePattern/MySqlHelper.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.StructuralDesignPatterns.FacadePattern;
+
+import java.sql.Connection;
+
+public class MySqlHelper 
+{
+	/**
+	 * Get MySql DB connection using connection parameters
+	 * @return
+	 */
+	public static Connection getMySqlDBConnection()
+	{
+		System.out.println("Connection Successfull");
+		return null;
+	}
+	
+	/**
+	 * Get data from table and generate pdf report
+	 * @param tableName
+	 * @param con
+	 */
+	public void generateMySqlPDFReport(String tableName, Connection con)
+	{
+		System.out.println("MySql PDF Report ");
+	}
+	
+	/**
+	 * Get data from table and generate pdf report
+	 * @param tableName
+	 * @param con
+	 */
+	public void generateMySqlHTMLReport(String tableName, Connection con)
+	{
+		System.out.println("MySql HTML Report ");
+	} 
+	
+	
+}
