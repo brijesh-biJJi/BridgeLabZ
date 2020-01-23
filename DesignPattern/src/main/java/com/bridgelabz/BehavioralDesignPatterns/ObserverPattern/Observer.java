3a3240dd0a9ba75f9f2068468db991ca6ccbaa6a
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-01-23 11:22:21 +0530

    [Brijesh] Add.Implemented Observer interface

---
+++ DesignPattern/src/main/java/com/bridgelabz/BehavioralDesignPatterns/ObserverPattern/Observer.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.BehavioralDesignPatterns.ObserverPattern;
+
+public interface Observer 
+{
+	/**
+	 * method to update the observer, used by subject
+	 */
+	public void update();
+		
+	/**
+	 * attach with subject to observe
+	 * @param sub
+	 */
+	public void setSubject(Subject sub);
+}
